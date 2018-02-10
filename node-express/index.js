fcbc8c655f074d68b6e484f7dded95f571de9366
Aniwat Phaphuangwittayakul <aniwat.pha@gmail.com>
2018-02-10 16:40:07 +0700

    Add promo and dish routes

--- node-express/index.js
+++ node-express/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -14,6 +15,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
 
 app.use(express.static(__dirname + '/public'));
 
