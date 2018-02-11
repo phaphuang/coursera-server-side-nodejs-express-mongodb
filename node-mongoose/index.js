8c377fdfc96aecdcc1ad489f70cc1094385c2c8e
Aniwat Phaphuangwittayakul <aniwat.pha@gmail.com>
2018-02-11 20:16:55 +0700

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -11,26 +11,24 @@ connect.then((client) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+
+        return db.collection('dishes').drop();
+    })
+    .then(() => {
+        return db.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({}).exec();
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return db.collection('dishes').drop();
-        })
-        .then(() => {
-            return db.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        })
 });
