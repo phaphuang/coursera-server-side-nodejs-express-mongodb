a511603e054b7fb0bbdf508a9656ed98336de0bd
Aniwat Phaphuangwittayakul <aniwat.pha@gmail.com>
2018-02-22 22:53:02 +0700

    HTTPS

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -33,6 +33,16 @@ connect.then((client) => {
 
 var app = express();
 
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    // return Status Code = 307
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
